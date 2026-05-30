ae78f1949aece763c9c2e91553d0eabf19d9c2ae
clive520 <39232044+clive520@users.noreply.github.com>
2026-05-30 21:45:33 +0800

    fix: import path in StudentLoginGrid

--- app/t/StudentLoginGrid.tsx
+++ app/t/StudentLoginGrid.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { studentLoginWithPassword } from "../../actions";
+import { studentLoginWithPassword } from "./actions";
 
 type Student = {
   id: string;
